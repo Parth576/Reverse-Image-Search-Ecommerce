de9b1d7987ca47f6276a69299e0974c40d13bfbb
Priyesh1202 <priyesh.vakharia@gmail.com>
2020-07-03 15:38:01 +0530

    Resolved API error

--- API/main.py
+++ API/main.py
@@ -3,7 +3,6 @@ import json
 from PIL import Image
 import cv2
 import numpy as np
-import base64
 
 app=Flask(__name__)
 
@@ -11,14 +10,23 @@ app=Flask(__name__)
 def classify():
     try:
         file = request.files['image']
+        print(file)
         img=Image.open(file.stream)
-        print(img.size)
-        print(np.array(img).shape)
-        img.save('fromReact.jpg')
-        cvimg=cv2.imread('fromReact.jpg',cv2.IMREAD_COLOR)
-        #cvimg = cv2.cvtColor(cvimg, cv2.COLOR_RGB2BGR)
-        print(cvimg.shape)
-        cv2.imshow('frame',cvimg)
+        # print(img.size)
+        # print(np.array(img).shape)
+        # img.save('fromReact.jpg')
+        # cvimg=cv2.imread('fromReact.jpg',cv2.IMREAD_COLOR)
+        # #cvimg = cv2.cvtColor(cvimg, cv2.COLOR_RGB2BGR)
+        # print(cvimg.shape)
+        # cv2.imshow('frame',cvimg)
+        imgg = np.array(img)
+        imcv = cv2.cvtColor(imgg,cv2.COLOR_RGB2BGR)
+        cv2.imwrite('newimg.jpg',imcv)
+        imgnew = cv2.imread('newimg.jpg')
+        print(imgnew.shape)
+        # cv2.imshow('New',imgnew)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
         return json.dumps({'status':'no error'})
     except Exception as e:
         print(str(e))
