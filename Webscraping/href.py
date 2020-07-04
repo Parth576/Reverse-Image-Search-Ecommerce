0558f18c716c23cb65935e7b902de89e8a6a152d
RiyaGupta99 <57893352+RiyaGupta99@users.noreply.github.com>
2020-07-04 14:39:11 +0530

    Updated details file

--- Webscraping/href.py
+++ Webscraping/href.py
@@ -6,31 +6,25 @@ source = requests.get('https://www.google.com/search?q=blue+jeans&hl=en&sxsrf=AL
 
 soup = BeautifulSoup(source,'lxml')
 
-#print(soup.prettify())
 
 everything = []
 
 for mydiv in soup.find_all('div',class_="P8xhZc"):
-    #print(mydiv.prettify()
-    my_href = mydiv.div.a['href']
-    # print(mydiv.div.a['href'])
+    link_href = mydiv.div.a['href']
     title = mydiv.div.a.text
-    #print(mydiv.div.a.text)
+    
     price = mydiv.find('span',class_ = 'HRLxBb').text
-    #print(price)
+    
     store = mydiv.find('div', class_="dD8iuc").text
-    #print(store)
+    
     store = store.split('from')[1]
-    #print(store)
-    data = {'my_href':my_href,'title':title,'price':price,'store':store}
-    #print(data)
+    
+    data = {'link_href':link_href,'title':title,'price':price,'store':store}
     everything.append(data)
 
 c=0
 for image in soup.find_all('div', class_="oR27Gd"):
-    # print(image.img['src'])
+    print(image.img['src'])
     c+=1
-
-print(c)
 
 #print(soup.find('div', class_="oR27Gd").img['src'])
