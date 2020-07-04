26ce726f905d07e614ed350b1ef5f47a0c220d94
Parth <rickingmorty@pop-os.localdomain>
2020-07-04 12:01:29 +0530

    v1 - webscraping api added, all results displayed in the react app

--- API/main.py
+++ API/main.py
@@ -9,14 +9,25 @@ import imutils
 import pickle
 from pyimagesearch.colordescriptor import ColorDescriptor
 from pyimagesearch.searcher import Searcher
+import os
+import base64
+from flask_cors import CORS, cross_origin
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
 
 app=Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 searchTerms=[]
 
 @app.route('/classify',methods=['POST'])
+@cross_origin()
 def classify():
 	global searchTerms
+	searchTerms=[]
 	try:
 		file = request.files['image']
 		print(file)
@@ -46,27 +57,45 @@ def classify():
 			searchTerms.append(mlb.classes_[j])
 			print(searchTerms)
 			label = "{}: {:.2f}%".format(mlb.classes_[j], proba[j] * 100)
-		return json.dumps({'Result':searchTerms,'Confidence':l})
+		url = 'https://www.google.co.in/search?hl=en&authuser=0&tbm=shop&sxsrf=ALeKk01dLFnuhG7sPOZGKQAxW7e25dQIdA%3A1593687520675&source=hp&ei=4L39XvbaJtie9QOgmpmYBw&q={}+{}&oq={}+{}&gs_lcp=Cgtwcm9kdWN0cy1jYxADMgQIIxAnMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAUNULWJ0aYKwcaABwAHgAgAGOAogBvwySAQUwLjcuM5gBAKABAaoBD3Byb2R1Y3RzLWNjLXdpeg&sclient=products-cc&ved=0ahUKEwj2yrPUtK7qAhVYT30KHSBNBnMQ4dUDCAc&uact=5'.format(searchTerms[1],searchTerms[0],searchTerms[1],searchTerms[0])
+		response = requests.get(url)
+
+		soup = BeautifulSoup(response.text,"html.parser")
+
+		scrapingResults=soup.findAll('img')[1:]
+		newResults=[]
+		for i in scrapingResults:
+			newResults.append(i['src'])
+		
+		return json.dumps({'Result':searchTerms,'Confidence':l,'scrapingResults':newResults})
 	except Exception as e:
 		print(str(e))
-		return json.dumps({'status':str(e)})
+		return json.dumps({'errorMessage':str(e)})
 
 @app.route('/reverseImageSearch',methods=['GET'])
+@cross_origin()
 def reverseImageSearch():
-	cd = ColorDescriptor((4, 8, 6))
+	try:
+		cd = ColorDescriptor((4, 8, 6))
 
-	query = cv2.imread('newimg.jpg')
-	features = cd.describe(query)
+		query = cv2.imread('newimg.jpg')
+		features = cd.describe(query)
 
-	searcher = Searcher('index.csv')
-	results = searcher.search(features)
+		searcher = Searcher('index.csv')
+		results = searcher.search(features)
 
-	
-	answer = [c for c in str(results[0][1]).split('.')]
-	print(answer)
-	answer = [a for a in str(answer[0]).split(':')]
-	print(answer)
-	return json.dumps('success')
+		
+		answer = [c for c in str(results[0][1]).split('.')]
+		print(answer)
+		imagePath=os.getcwd()+'/reversedata/'+answer[0]+'.png'
+		img=cv2.imread(imagePath)
+		ret,stream=cv2.imencode('.png',img)
+		encoded_img=base64.b64encode(stream).decode()
+		img_base64="data:image/png;base64,"+encoded_img	
+		return json.dumps({'matchedImage':img_base64})
+	except Exception as e:
+		print(e)
+		return json.dumps({'errorMessage':str(e)})
 	
 
 if __name__=='__main__':
--- React/src/components/image-upload/upload.component.jsx
+++ React/src/components/image-upload/upload.component.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 class Upload extends React.Component {
 
-    state={image:null};
+    state={image:null,matchedImage:null,Result:[],Confidence:[],scrapedResults:[]};
+
+    reverseImageSearch = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/reverseImageSearch',{
+                method:'GET'
+            });
+            const res = await response.json();
+            if(res.errorMessage===undefined) {
+                this.setState({matchedImage:res.matchedImage});
+            }
+            else {
+                alert(res.errorMessage);
+            }
+        }
+        catch(err) {
+            console.log(err);
+        }
+    };
 
     uploadHandler = async e => {
         const formData = new FormData();
@@ -10,10 +30,14 @@ class Upload extends React.Component {
         try {
             const response = await fetch('http://localhost:5000/classify',{
                 method:'POST',
-                body:formData
+                body:formData,
+                
             });
             const res = await response.json();
-            console.log(res);
+            if(res.errorMessage===undefined) {
+                this.setState({Result:res.Result,Confidence:res.Confidence,scrapedResults:res.scrapingResults});
+            }
+            await this.reverseImageSearch();
         }
         catch(err) {
             console.log(err);
@@ -22,8 +46,26 @@ class Upload extends React.Component {
 
     render() {
         return (
-            <div style={{margin:20,padding:20,alignItems:'center'}}>
+            <div style={{margin:20,padding:20,alignItems:'center',textAlign:'center'}}>
                 <input type="file" id="uploader" onChange={e=>this.uploadHandler(e)}/>
+                <br/>
+                <img style={{marginTop:"10%"}} src={this.state.matchedImage} alt="Matched Image"/>
+                <div style={{width:200,height:400,textAlign:'center',alignItems:'center',marginLeft:"45%",marginTop:"10%"}}>
+                <Carousel showIndicators={false}>
+                    {this.state.scrapedResults.map(elem=>{
+                        return <div style={{width:200,height:300}}><img alt="scraped results" src={elem}/></div>
+                    })}
+                </Carousel>
+                </div>
+                <br/>
+                <div>
+                <h1 style={{marginTop:"10%"}} className="f4 bold center mw5">Classification Result</h1>
+                <ul className="list pl0 ml0 center mw5 ba b--light-silver br3">
+                    {this.state.Result.map(el=>{
+                        return <li key={el} className="ph3 pv2">{el}</li>
+                    })}
+                </ul>
+                </div>
             </div>
         );
     }
