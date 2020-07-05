effb138d2f268a85cbeff51098669ac2a9b01a8e
Parth <rickingmorty@pop-os.localdomain>
2020-07-05 09:50:57 +0530

    added build and requirements

---
+++ React/build/precache-manifest.b4fd8198761e2b7a0f9c066e1d92745a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b1d3051927161b448b553d26131c9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a236e447aa7bd9831df",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "ccade45c26e243442ac6",
+    "url": "/static/css/main.6e7ea207.chunk.css"
+  },
+  {
+    "revision": "2a236e447aa7bd9831df",
+    "url": "/static/js/2.faf5ef30.chunk.js"
+  },
+  {
+    "revision": "996e97a9c59b2ad74d9e36e982ab30fe",
+    "url": "/static/js/2.faf5ef30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccade45c26e243442ac6",
+    "url": "/static/js/main.c724416c.chunk.js"
+  },
+  {
+    "revision": "9dce950c1f8d5f2c45b4",
+    "url": "/static/js/runtime-main.a4da908e.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
